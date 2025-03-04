2c4a62031e1815da054d9909886095c9aab0ac67
(muhammadirgiefalssandi) <(falssandiirgi@gmail.com)>
2025-03-04 11:42:23 +0700

    introduction C++

---
+++ pertemuan2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char jeniskelamin = 'L';
+    string janis = "laki-laki";
+    float x = 10.5;
+    int y = 5;
+    bool check = true;
+
+    cout << "selamat datang di TI UMY\n";
+    cout << "Yogyakarta" << endl;
+    cout << "Indonesia";
+}
